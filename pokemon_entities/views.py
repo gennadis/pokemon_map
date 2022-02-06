b354d0b6d937c9a84a5e2383354b5abd0c25d88f
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-06 18:51:08 +0300

    Display PokemonEntities on main page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,7 +4,7 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-from pokemon_entities.models import Pokemon
+from pokemon_entities.models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -29,37 +29,24 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-
-    with open("pokemon_entities/pokemons.json", encoding="utf-8") as database:
-        pokemons = json.load(database)["pokemons"]
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon["entities"]:
-            add_pokemon(
-                folium_map,
-                pokemon_entity["lat"],
-                pokemon_entity["lon"],
-                pokemon["img_url"],
-            )
 
-    pokemons_on_page = []
-    for pokemon in Pokemon.objects.all():
-        if pokemon.image:
-            pokemons_on_page.append(
-                {
-                    "pokemon_id": pokemon.pk,
-                    "img_url": pokemon.image.url,
-                    "title_ru": pokemon.title,
-                }
-            )
-        else:
-            pokemons_on_page.append(
-                {
-                    "pokemon_id": pokemon.pk,
-                    "title_ru": pokemon.title,
-                }
-            )
+    for pokemon_entity in PokemonEntity.objects.all():
+        add_pokemon(
+            folium_map,
+            pokemon_entity.lat,
+            pokemon_entity.lon,
+            request.build_absolute_uri(pokemon_entity.pokemon.image.url),
+        )
+
+    pokemons_on_page = [
+        {
+            "pokemon_id": pokemon.pk,
+            "img_url": pokemon.image.url,
+            "title_ru": pokemon.title,
+        }
+        for pokemon in Pokemon.objects.all()
+    ]
 
     return render(
         request,
